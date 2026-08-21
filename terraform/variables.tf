c40dcba25a39398d7903218c6ad32e717c99d6b8
lazzerex <nambinh236@gmail.com>
2026-08-21 18:16:33 +0700

    add project resource naming config

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,16 @@ variable "local_emulator" {
   type        = bool
   default     = false
 }
+
+variable "project_name" {
+  description = "Name prefix for every Parcel resource."
+  type        = string
+  default     = "parcel"
+}
+
+locals {
+  bucket_name = "${var.project_name}-files"
+  table_name  = "${var.project_name}-metadata"
+  queue_name  = "${var.project_name}-jobs"
+  dlq_name    = "${var.project_name}-jobs-dlq"
+}
